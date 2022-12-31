71026ad388127d89379e6cde445bb318b117f016
Ashish Kumar <at854604@gmail.com>
2023-01-01 00:27:37 +0530

    build

---
+++ build/precache-manifest.11d1dc06107f97fb4b9317eb0ad81ae4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540130b770229159b7b165c3a2f23fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4abde0debb6d80c935b9",
+    "url": "/static/css/main.12c3bfa5.chunk.css"
+  },
+  {
+    "revision": "a51509765a3a07187988",
+    "url": "/static/js/2.d5e51f7b.chunk.js"
+  },
+  {
+    "revision": "4abde0debb6d80c935b9",
+    "url": "/static/js/main.b7e464c4.chunk.js"
+  },
+  {
+    "revision": "e70dac4ba32196750187",
+    "url": "/static/js/runtime~main.5987751f.js"
+  }
+]);
